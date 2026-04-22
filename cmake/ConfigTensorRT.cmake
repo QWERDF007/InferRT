4ada0d645e2c48bad9d00c10ca8f1592f818e852
QWERDF007 <595517713@qq.com>
2026-04-22 16:42:40 +0800

    build: 修复构建找不到 TRT_VERSION

--- cmake/ConfigTensorRT.cmake
+++ cmake/ConfigTensorRT.cmake
@@ -1,12 +1,8 @@
 # Set TensorRT root directory
 # You can override this by setting TRT_ROOT environment variable
 # or passing -DTensorRT_ROOT=/path/to/tensorrt to CMake
-if(NOT DEFINED TRT_ROOT)
-  set(TRT_ROOT "D:/Software/TensorRT-10.16.0.72" CACHE PATH "TensorRT installation directory")
-  set(TRT_VERSION "10.16.0.72" CACHE STRING "TensorRT version")
-endif()
+set(TRT_ROOT "D:/Software/dev/TensorRT-10.16.1.11" CACHE PATH "TensorRT installation directory" FORCE)
+set(TRT_VERSION "10.16.1.11" CACHE STRING "TensorRT version" FORCE)
 
 # Include FindTensorRT to locate and configure TensorRT
-if(NOT TARGET TensorRT::TensorRT)
-    include(${CMAKE_CURRENT_LIST_DIR}/FindTensorRT.cmake)
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/FindTensorRT.cmake)
