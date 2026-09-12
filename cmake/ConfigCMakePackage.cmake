1b1e0c58e691dfd8bca1e355c865171e00c4080a
QWERDF007 <595517713@qq.com>
2026-09-12 12:08:47 +0800

    fix: 封装后端实现依赖为私有链接，修复安装包泄露 TensorRT 与 Faiss

--- cmake/ConfigCMakePackage.cmake
+++ cmake/ConfigCMakePackage.cmake
@@ -37,24 +37,6 @@ set(_INFERRT_PACKAGE_CONFIG_FILES
     "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
 )
 
-if(${PROJECT_NAME_UPPER}_ENABLE_CUDA AND ${PROJECT_NAME_UPPER}_BUILD_TENSORRT)
-    list(APPEND _INFERRT_PACKAGE_CONFIG_FILES "${CMAKE_CURRENT_LIST_DIR}/FindTensorRT.cmake")
-    list(APPEND _INFERRT_PACKAGE_CONFIG_FILES "${CMAKE_CURRENT_LIST_DIR}/ConfigDependencyDefaults.cmake")
-    # features has a public Faiss target in its exported link interface. Keep
-    # the resolver in every package that can export features, even if a
-    # future build discovers Faiss through an external CMake package instead
-    # of the legacy variables used by the build tree.
-    list(APPEND _INFERRT_PACKAGE_CONFIG_FILES "${CMAKE_CURRENT_LIST_DIR}/ConfigFaiss.cmake")
-endif()
-
-if(${PROJECT_NAME_UPPER}_BUILD_ONNX)
-    list(APPEND _INFERRT_PACKAGE_CONFIG_FILES "${CMAKE_CURRENT_LIST_DIR}/ConfigONNXRuntime.cmake")
-endif()
-
-if(${PROJECT_NAME_UPPER}_BUILD_OPENVINO)
-    list(APPEND _INFERRT_PACKAGE_CONFIG_FILES "${CMAKE_CURRENT_LIST_DIR}/ConfigOpenVINO.cmake")
-endif()
-
 install(FILES ${_INFERRT_PACKAGE_CONFIG_FILES} DESTINATION "${INFERRT_INSTALL_CMAKEDIR}")
 
 unset(_inferrt_component)
